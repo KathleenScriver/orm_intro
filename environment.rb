c6f4584e8cdf501d0d1cd0d7084602db4148e341
KathleenScriver <klescriver@gmail.com>
2019-09-04 13:15:56 -0600

    Adds save and create methods.

--- environment.rb
+++ environment.rb
@@ -1,5 +1,6 @@
 require 'sqlite3'
-require_relative '../lib/song.rb'
+require 'pry'
+require_relative './lib/song.rb'
 
 DB = {:conn => SQLite3::Database.new("db/music.db")}
 # can now access DB[:conn] fom songs.rb
--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,29 @@
 class Song
-  attr_accessor :name, :album
+  attr_accessor :title, :album
 
-  def initialize(title, album)
+  def initialize(title, album, id = nil)
+    @id = id
     @title =  title
     @album = album
   end
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        title TEXT,
+        album TEXT
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
 
-
-
+  def save
+    sql = <<-SQL
+      INSERT INTO songs (title, album)
+      VALUES (?, ?)
+    SQL
+    binding.pry
+    DB[:conn].execute(sql, self.title, self.album)
+  end
 end
